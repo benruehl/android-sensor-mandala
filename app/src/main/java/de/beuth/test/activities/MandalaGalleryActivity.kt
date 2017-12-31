6272b4fc651b0251ea2a688e1c526af786b17b24
benruehl <dev.ruehl@gmail.com>
2017-12-31 18:29:48 +0100

    Improve some UI parts

--- app/src/main/java/de/beuth/test/activities/MandalaGalleryActivity.kt
+++ app/src/main/java/de/beuth/test/activities/MandalaGalleryActivity.kt
@@ -2,6 +2,8 @@ package de.beuth.test.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.View.GONE
 import android.widget.ArrayAdapter
 import android.widget.GridView
 import de.beuth.test.R
@@ -16,10 +18,13 @@ class MandalaGalleryActivity : AppCompatActivity() {
 
     private val galleryGrid: GridView by bind(R.id.mandalaGalleryGrid)
 
+    private val galleryEmptyPlaceholder: View by bind(R.id.mandalaGalleryEmptyPlaceholder)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mandala_gallery)
 
+        initEmptyGalleryPlaceholder()
         initGalleryGrid()
     }
 
@@ -27,5 +32,10 @@ class MandalaGalleryActivity : AppCompatActivity() {
         val persistedMandalas = DAOFactoryService.daoFactory.getMandalaDAO().findAll().sortedByDescending { m -> m.creationDate }
 
         galleryGrid.adapter = MandalaGalleryGridAdapter(this, android.R.layout.simple_gallery_item, persistedMandalas)
+    }
+
+    private fun initEmptyGalleryPlaceholder() {
+        if (DAOFactoryService.daoFactory.getMandalaDAO().findAll().isNotEmpty())
+            galleryEmptyPlaceholder.visibility = GONE
     }
 }
